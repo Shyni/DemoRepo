194da7e91be001879cab4a8499cd9bbb19eeeb98
Arun <arunchauhan54@gmail.com>
2018-09-14 14:15:20 +1000

    Addded Searchpage.java and Modifies LoginPage.java

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -4,4 +4,8 @@ public class LoginPage {
 	public void login(){
 		System.out.println("LoginPage.java : login method");
 	}
+	
+	public void loginSet(){
+		System.out.println("LoginPage.java : loginSet method");
+	}
 }
